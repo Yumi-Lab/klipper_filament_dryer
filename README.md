816fa4340e51a714df3059e80cba2ba05dc71d4d
Maxime3d77 <114477279+Maxime3d77@users.noreply.github.com>
2025-04-29 06:05:08 +0200

    Yms Dryer

--- README.md
+++ README.md
@@ -1,78 +1,58 @@
 # Klipper Filament Dryer
 
-## ** CURRENTLY IN BETA **
-
 ## Introduction
 
-Klipper add-on to automatically turn on a filament dryer heater based on the humidity level or manually for a specified amount of time. An optional can also be opened/closed on an interval.
+Module for YMS dryers
 
 ## Sample Config
 
-<pre>[filament_dryer filament_dryer]
-interval: 1
-sensor: dryer_sensor
-heater: dryer_heater
-target_humidity: 30
-auto_target_temp: 60
-manual_target_temp: 80
-default_manual_dry_time: 45
-auto_dry_time: 30
-manual_dryer_on_macro: MANUAL_DRYER_ON_MACRO
-auto_dryer_on_macro: AUTO_DRYER_ON_MACRO
-dryer_off_macro: DRYER_OFF_MACRO
-vent_interval: 2
-vent_length: 3
-vent_start_macro: OPEN_VENT
-vent_end_macro: CLOSE_VENT</pre>
+<pre>[filament_dryer YMS-1]
+heater: YMS-1
+auto_target_temp: 45  # Température automatique par défaut si besoin
+manual_target_temp: 45  # Température pour commande DRY_FILAMENT si pas de TEMP spécifié
+default_manual_dry_time: 60  # Durée par défaut en minutes si pas précisé
+
+
+[heater_generic YMS-1]
+heater_pin: smartbox:PC8 
+sensor_type: EPCOS 100K B57560G104F #ATC Semitec 104GT-2
+sensor_pin: smartbox:PC1
+#sensor_list: temperature_sensor dryer_sensor
+#maximum_deviation: 999
+#combination_method: min
+max_power: 1
+control: pid #watermark
+pid_Kp: 50
+pid_Ki: 50
+pid_Kd: 50
+#kick_start_time: 0.5
+#pwm_cycle_time:
+#max_delta: 3.0
+min_temp: -50
+max_temp: 110</pre>
 
 ## Requirements
 
-- Humidity/Temperature Sensor such as BME280/680
 - Heater
-- Vent (optional)
 
-## Additional Config Needed
+## Macros
 
-The following configs are also required for the heater and temperature sensor:
+<pre>[gcode_macro START_DRYER_YMS1]
+description: "Démarrer séchage YMS-1 avec température et minutes personnalisées"
+gcode:
+    {% set target_temp = params.TEMP|default(45)|float %}
+    {% set minutes = params.MINUTES|default(60)|float %}
 
-<pre>[heater_generic dryer_heater]
-heater_pin: dryer:gpio6
-sensor_type: temperature_combined
-sensor_list: temperature_sensor dryer_sensor
-maximum_deviation: 999
-combination_method: min
-control: watermark
-max_delta: 3.0
-min_temp: 0
-max_temp: 100
+    DRY_FILAMENT MINUTES={minutes|int} TEMP={target_temp|int}
 
-[temperature_sensor dryer_sensor]
-sensor_type: BME280
-i2c_bus: i2c3_PB3_PB4
-i2c_address: 119
 
-[heater_fan dryer_fan]
-pin: dryer:gpio14
-max_power: 0.5
-off_below: 0.31
-heater: dryer_heater
-heater_temp: 25
-shutdown_speed: 0</pre>
+[gcode_macro STOP_DRYER_YMS1]
+description: "Arrêter manuellement le sèche-filament YMS-1"
+gcode:
+    STOP_FILAMENT_DRYER
+    M118 Dryer : Séchage arrêté manuellement.</pre>
 
-The BME280 config above will depend on the MCU you're using. Please refer to the [Klipper Config Reference](https://www.klipper3d.org/Config_Reference.html#bmp180bmp280bme280bme680-temperature-sensor) or [Klipper Discord](https://discord.klipper3d.org/) for help on configuring this sensor.
 
-Note that a heater_fan is added to ensure the fan turns on with the heater and turns off when the dryer chamber temp is below a certain threshold (in this case, 25 degrees). Max_power should also be adjusted to your needs. If using more than 1 fan, consider configuring these as [multi_pin](https://www.klipper3d.org/Config_Reference.html#multi_pin):
-
-<pre>[multi_pin multi_dryer_fans]
-pins: dryer:gpio14,dryer:gpio13
-
-[heater_fan dryer_fans]
-pin: multi_pin:multi_dryer_fans
-max_power: 0.5
-off_below: 0.31
-heater: dryer_heater
-heater_temp: 25
-shutdown_speed: 0</pre>
 
 ## Install
 
@@ -87,143 +67,3 @@ cd klipper_filament_dryer
 sudo rm -r ~/klipper_filament_dryer
 sudo rm -r ~/klipper/klippy/extras/filament_dryer.py</pre>
 
-## Settings
-
-- **interval**: How often to check the humidity level
-- **sensor**: Sensor name of the humidity/temperature sensor
-- **heater**: heater to control
-- **target_humidity**: Target humidity. If humidity is above the target, the heater will be enabled. Once the humidity is below the target, the heater is turned off
-- **auto_target_temp**: Temperature the heater will be set to when automatically enabled
-- **manual_target_temp**: Temperature the heater will be set to when manually enabled
-- **default_manual_dry_time**: When manually enabling the dryer, the heater will be enabled for this amount of minutes unless specified otherwise
-- **auto_dry_time**: When automatically enabling the dryer based on the humidity, setting this value to non-zero will force the dryer to remain enabled for this amount of minutes
-- **manual_dryer_on_macro**: name of macro to execute when heater is manually enabled
-- **auto_dryer_on_macro**: name of macro to execute when heater is manually enabled
-- **dryer_off_macro**:  name of macro to execute when heater is disabled
-- **vent_interval**: interval in minutes to execute the vent open macro (see vent_start_macro)
-- **vent_length**: length of time in seconds before executing the vent close macro (see vent_end_macro)
-- **vent_start_macro**: name of macro to execute to open the vent
-- **vent_end_macro**:  name of macro to execute to close the vent
-
-## Manual Filament Drying
-
-The dryer can be manually enabled for the default dry time and temperature by running the following gcode command: DRY_FILAMENT
-To dry the filament for a custom time and temperature: DRY_FILAMENT MINUTES=120 TEMP=80
-
-Alternatively, you can add this macro to printer.cfg to have it show up in your list of Macros in Fluidd/Mainsail:
-
-<pre>[gcode_macro DRY_FILAMENT]
-rename_existing: BASE_DRY_FILAMENT
-gcode:
-    {% set minutes = params.MINUTES | default(60) %}
-    {% set temp = params.TEMP | default(70) %}
-    BASE_DRY_FILAMENT MINUTES={minutes} TEMP={temp}</pre>
-
-![image](https://github.com/Turge08/klipper_filament_dryer/assets/6312320/969f4eec-ba11-4fab-a9bb-ace189d2fd9f)
-
-To stop the filament dryer, you can execute STOP_FILAMENT_DRYER or use the following macro:
-
-<pre>[gcode_macro STOP_FILAMENT_DRYER]
-rename_existing: BASE_STOP_FILAMENT_DRYER
-gcode:
-    BASE_STOP_FILAMENT_DRYER</pre>
-
-## Misc
-
-Running the GCode command "GET_FILAMENT_DRYER_INFO" will return information about the dryer including the time remaining if the heater is enabled:
-
-<pre>// Humidity: 26
-// Temperature: 23
-// Target Temp: 60
-// Dry Mode: Manual
-// Dry Time Left: 0:00:57</pre>
-
-## Example
-
-My test filament dryer includes all controlled by a BTT EBB42 can bus toolhead PCB as the "dryer" mcu:
-
-- a 24V 300W heater with a 24V fan
-- a BME280 temperature and humidity sensor
-- an iris vent controlled with a FS90R servo with another 24V fan
-
-Full config:
-
-<pre>[filament_dryer filament_dryer]
-interval: 1
-sensor: dryer_sensor
-heater: dryer_heater 
-target_humidity: 30
-auto_target_temp: 60
-manual_target_temp: 70
-default_manual_dry_time: 120
-auto_dry_time: 0
-#manual_dryer_on_macro: MANUAL_DRYER_ON_MACRO
-#auto_dryer_on_macro: AUTO_DRYER_ON_MACRO
-#dryer_off_macro: DRYER_OFF_MACRO
-vent_interval: 30
-vent_length: 5
-vent_start_macro: OPEN_VENT
-vent_end_macro: CLOSE_VENT
-
-[servo dryer_vent]
-pin: dryer:PB9
-initial_angle: 180
-maximum_servo_angle: 250
-
-[heater_fan dryer_fan]
-pin: dryer:PA1
-max_power: 0.5
-off_below: 0.31
-heater: dryer_heater
-heater_temp: 25
-shutdown_speed: 0
-
-[fan_generic vent_fan]
-pin: dryer:PA0
-max_power: 1
-
-[heater_generic dryer_heater]
-heater_pin: dryer:PA2
-sensor_type: temperature_combined
-sensor_list: temperature_sensor dryer_sensor
-maximum_deviation: 999
-combination_method: min
-control: watermark
-max_delta: 3.0
-min_temp: 0
-max_temp: 100
-
-[temperature_sensor dryer_sensor]
-sensor_type: BME280
-i2c_bus: i2c3_PB3_PB4
-i2c_mcu: dryer
-
-[gcode_macro OPEN_VENT]
-gcode:
-    #SET_SERVO SERVO=dryer_vent ANGLE=270
-    SET_FAN_SPEED FAN=vent_fan SPEED=1
-    SET_SERVO SERVO=dryer_vent ANGLE=130
-    G4 P900
-    SET_SERVO SERVO=dryer_vent ANGLE=180
-    SET_SERVO SERVO=dryer_vent WIDTH=0
-
-[gcode_macro CLOSE_VENT]
-gcode:
-    SET_SERVO SERVO=dryer_vent ANGLE=230
-    G4 P900
-    SET_SERVO SERVO=dryer_vent ANGLE=180
-    SET_SERVO SERVO=dryer_vent WIDTH=0
-    SET_FAN_SPEED FAN=vent_fan SPEED=0
-
-[gcode_macro DRY_FILAMENT]
-rename_existing: BASE_DRY_FILAMENT
-gcode:
-    {% set minutes = params.MINUTES | default(120) %}
-    {% set temp = params.TEMP | default(70) %}
-    BASE_DRY_FILAMENT MINUTES={minutes} TEMP={temp}
-
-[gcode_macro STOP_FILAMENT_DRYER]
-rename_existing: BASE_STOP_FILAMENT_DRYER
-gcode:
-    BASE_STOP_FILAMENT_DRYER
-</pre>
--- src/filament_dryer.py
+++ src/filament_dryer.py
@@ -1,3 +1,4 @@
+
 from datetime import timedelta
 
 class filament_dryer:
@@ -5,7 +6,7 @@ class filament_dryer:
         self.name = config.get_name().split()[-1]
         self.printer = config.get_printer()
         self.interval = config.getint('interval',1,1,60)
-        self.sensor_name = config.get('sensor')
+        self.sensor_name = config.get('sensor', None)
         reactor = self.printer.get_reactor()
         self.dry_target_time = reactor.monotonic()
         self.vent_target_time = reactor.monotonic()
@@ -48,103 +49,32 @@ class filament_dryer:
         self.printer.register_event_handler("klippy:ready",
             self.handle_ready)
 
-    def venting_callback(self, eventtime):
-        reactor = self.printer.get_reactor()
-        if self.vent_mode == "Off":
-            return eventtime + self.interval
-        if self.heater.target_temp == 0 and self.vent_mode != "Off":
-            if self.vent_end_macro and self.vent_mode == "Open":
-                self.gcode.respond_info("Executing %s" % (self.vent_end_macro))
-                self.gcode.run_script_from_command(self.vent_end_macro)
-            self.vent_target_time = reactor.monotonic()
-            self.vent_mode = "Off"
-            return eventtime + self.interval
-        if reactor.monotonic() > self.vent_target_time and self.vent_mode == "Open":
-            self.vent_mode = "Closed"
-            if self.vent_end_macro:
-                self.gcode.respond_info("Executing %s" % (self.vent_end_macro))
-                self.gcode.run_script_from_command(self.vent_end_macro)
-            self.vent_target_time = reactor.monotonic() + self.vent_interval * 60
-        elif reactor.monotonic() > self.vent_target_time and self.vent_mode == "Closed":
-            self.vent_mode = "Open"
-            if self.vent_start_macro:
-                self.gcode.respond_info("Executing %s" % (self.vent_start_macro))
-                self.gcode.run_script_from_command(self.vent_start_macro)
-            self.vent_target_time = reactor.monotonic() + self.vent_length
-        return eventtime + self.interval
-
-    def callback(self, eventtime):
-        reactor = self.printer.get_reactor()
-        if self.heater.target_temp == 0 and self.dry_mode != "Off":
-            self.dry_mode = "Off"
-            if self.dryer_off_macro:
-                self.gcode.respond_info("Executing %s" % (self.dryer_off_macro))
-                self.gcode.run_script_from_command(self.dryer_off_macro)
-        if reactor.monotonic() > self.dry_target_time or self.heater.target_temp == 0:
-            if self.sensor.humidity > self.target_humidity and self.dryer_enabled:
-                if self.heater.target_temp == 0:
-                    self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%i" % (self.heater_name, self.auto_target_temp))
-                    self.dry_mode = "Auto"
-                    if self.auto_dry_time > 0:
-                        self.gcode.respond_info("Current humidity: %i, Target humidity: %i. Turning on heater for %i minutes" % (self.sensor.humidity, self.target_humidity, self.auto_dry_time))
-                        self.dry_target_time = reactor.monotonic() + self.auto_dry_time * 60
-                    else:
-                        self.gcode.respond_info("Current humidity: %i, Target humidity: %i. Turning on heater" % (self.sensor.humidity, self.target_humidity))
-                        self.dry_target_time = reactor.monotonic()
-                    if self.vent_interval > 0:
-                        self.vent_target_time = reactor.monotonic() + self.vent_interval * 60
-                        self.vent_mode = "Closed"
-                    if self.auto_dryer_on_macro:
-                        self.gcode.respond_info("Executing %s" % (self.auto_dryer_on_macro))
-                        self.gcode.run_script_from_command(self.auto_dryer_on_macro)
-            else:
-                if self.dry_mode != "Off" and self.heater.target_temp != 0:
-                    self.gcode.respond_info("Current humidity: %i, Target humidity: %i. Turning off heater." % (self.sensor.humidity, self.target_humidity))
-                    self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=0" % (self.heater_name))
-        return eventtime + self.interval
-
     def handle_connect(self):
         try:
             self.heater = self.printer.lookup_object('heater_generic ' + self.heater_name)
         except Exception:
             raise self.printer.config_error("Could not find 'heater_generic [%s]' heater definition" % (self.heater_name))
         try:
-            self.sensor = self.printer.lookup_object('bme280 ' + self.sensor_name)
+            if self.sensor_name:
+                self.sensor = self.printer.lookup_object('bme280 ' + self.sensor_name)
+            else:
+                self.sensor = None
         except Exception:
-            raise self.printer.config_error("Could not find 'bme280 [%s]' sensor definition" % (self.sensor_name))
+            self.gcode.respond_info("No humidity sensor found, proceeding without it.")
+            self.sensor = None
 
     def handle_ready(self):
-        reactor = self.printer.get_reactor()
-        reactor.register_timer(self.callback, reactor.monotonic() + self.interval)
-        if self.vent_interval > 0:
-            reactor.register_timer(self.venting_callback, reactor.monotonic() + self.interval)
-        if self.auto_target_temp > self.heater.max_temp - 2:
-            raise self.printer.config_error("auto_target_temp of %s needs to be at least 2 degrees below heater max temp of %s" % (self.auto_target_temp, self.heater.max_temp))
-        if self.manual_target_temp > self.heater.max_temp - 2:
-            raise self.printer.config_error("manual_target_temp of %s needs to be at least 2 degrees below heater max temp of %s" % (self.manual_target_temp, self.heater.max_temp))
-        self.gcode.respond_info("Max Heater Temp: %f" % (self.heater.max_temp))
+        pass  # Simplified for heating only
 
     cmd_GET_FILAMENT_DRYER_INFO_help = "Get Filament Dryer Info"
     def cmd_GET_FILAMENT_DRYER_INFO(self, gcmd):
-        self.gcode.respond_info("Humidity: %i" % (self.sensor.humidity))
-        self.gcode.respond_info("Temperature: %i" % (self.sensor.temp))
+        if self.sensor:
+            self.gcode.respond_info("Humidity: %i" % (self.sensor.humidity))
+            self.gcode.respond_info("Temperature: %i" % (self.sensor.temp))
+        else:
+            self.gcode.respond_info("Humidity sensor not available.")
         self.gcode.respond_info("Target Temp: %i" % (self.heater.target_temp))
         self.gcode.respond_info("Dry Mode: %s" % (self.dry_mode))
-        seconds = self.dry_target_time - self.printer.get_reactor().monotonic()
-        if self.dry_mode == "Off":
-            seconds = 0
-        mm, ss = divmod(seconds, 60)
-        hh, mm = divmod(mm, 60)
-        self.gcode.respond_info("Dry Time Left: %i:%02i:%02i" % (hh,mm,ss))
-
-        self.gcode.respond_info("Vent Mode: %s" % (self.vent_mode))
-        seconds = self.vent_target_time - self.printer.get_reactor().monotonic()
-        if self.vent_mode == "Off":
-            seconds = 0
-        if self.vent_mode != "Disabled":
-            mm, ss = divmod(seconds, 60)
-            hh, mm = divmod(mm, 60)
-            self.gcode.respond_info("Vent Time Left: %i:%02i:%02i" % (hh,mm,ss))
 
     cmd_DRY_FILAMENT_help = "Dries filament for XX minutes"
     def cmd_DRY_FILAMENT(self, gcmd):
@@ -155,18 +85,9 @@ class filament_dryer:
             return
         reactor = self.printer.get_reactor()
         self.dry_target_time = reactor.monotonic() + self.dry_time * 60
-        if self.vent_interval > 0:
-            self.vent_target_time = reactor.monotonic() + self.vent_interval * 60
-            self.vent_mode = "Closed"
-        if self.dry_time == 0:
-            self.gcode.respond_info("Turning off manual drying")
-        else:
-            self.gcode.respond_info("Drying filament for %i minutes" % (self.dry_time))
-            self.dry_mode = "Manual"
-            self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%i" % (self.heater_name, target_temp))
-            if self.manual_dryer_on_macro:
-                self.gcode.respond_info("Executing %s" % (self.manual_dryer_on_macro))
-                self.gcode.run_script_from_command(self.manual_dryer_on_macro)
+        self.gcode.respond_info("Drying filament for %i minutes" % (self.dry_time))
+        self.dry_mode = "Manual"
+        self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%i" % (self.heater_name, target_temp))
 
     cmd_DISABLE_FILAMENT_DRYER_help = "Disables automatic filament dryer"
     def cmd_DISABLE_FILAMENT_DRYER(self, gcmd):
@@ -174,7 +95,7 @@ class filament_dryer:
         self.dryer_enabled = False
         self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=0" % (self.heater_name))
 
-    cmd_ENABLE_FILAMENT_DRYER_help = "Enabled automatic filament dryer"
+    cmd_ENABLE_FILAMENT_DRYER_help = "Enable automatic filament dryer"
     def cmd_ENABLE_FILAMENT_DRYER(self, gcmd):
         self.gcode.respond_info("Enabling automatic filament dryer")
         self.dryer_enabled = True
@@ -182,20 +103,6 @@ class filament_dryer:
     cmd_STOP_FILAMENT_DRYER_help = "Stops filament dryer heater"
     def cmd_STOP_FILAMENT_DRYER(self, gcmd):
         self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=0" % (self.heater_name))
-
-    def get_status(self, eventtime):
-        try:
-            seconds = self.dry_target_time - self.printer.get_reactor().monotonic()
-            if self.dry_mode == "Off":
-                seconds = 0
-            mm, ss = divmod(seconds, 60)
-            hh, mm = divmod(mm, 60)
-            return {
-                'name': self.name,
-                'config': "{'target_humidity': '%f', 'interval': '%i'}" % (self.target_humidity, self.interval),
-                'info': "{'humidity': '%i', 'temperature': '%i', 'dry_mode': '%s', 'dry_time': '%i:%02i:%02i'}" % (self>            }
-        except Exception:
-            return { }
 
 def load_config_prefix(config):
     return filament_dryer(config)
